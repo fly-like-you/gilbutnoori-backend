d38f7f73730e2af9d1df9a8bac21710d25834677
fly-like-you <qkrwnsgh71w@gmail.com>
2024-11-04 00:29:40 +0900

    feat. 코스에 둘렛길 추가

--- src/main/java/com/ssafy/gilbut/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/ssafy/gilbut/advice/ExceptionControllerAdvice.java
@@ -1,31 +1,32 @@
 package com.ssafy.gilbut.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionControllerAdvice {
 
 	@ExceptionHandler(Exception.class)
-	public String handleException(Exception ex, Model model) {
+	public ResponseEntity<?> handleException(Exception ex, Model model) {
 		log.error("Exception 발생 : {}", ex.getMessage());
+		ex.printStackTrace();
 		model.addAttribute("msg", "처리중 에러 발생!!!");
-		return "error/error";
+		return ResponseEntity.badRequest().body(ex.getMessage());
 	}
 
 	@ExceptionHandler(NoHandlerFoundException.class)
 	@ResponseStatus(value = HttpStatus.NOT_FOUND)
-	public String handle404(NoHandlerFoundException ex, Model model) {
+	public ResponseEntity<?> handle404(NoHandlerFoundException ex, Model model) {
 		log.error("404 발생 : {}", "404 page not found");
 		model.addAttribute("msg", "해당 페이지를 찾을 수 없습니다!!!");
-		return "error/error";
+		return null;
 	}
 
 }
--- src/main/java/com/ssafy/gilbut/course/service/CourseServiceImpl.java
+++ src/main/java/com/ssafy/gilbut/course/service/CourseServiceImpl.java
@@ -1,7 +1,39 @@
 package com.ssafy.gilbut.course.service;
 
+import com.ssafy.gilbut.course.mapper.CourseMapper;
+import com.ssafy.gilbut.course.model.dto.CourseDTO;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService{
+    private final CourseMapper courseMapper;
+    @Override
+    public Page<Map<String, Object>> courseList(Map<String, Object> paramMap, Pageable page) {
+        paramMap.put("offset", page.getOffset());
+        paramMap.put("pageSize",page.getPageSize());
+
+        log.trace("offset = {}, pageSize = {}", page.getOffset(), page.getPageSize());
+
+        List<Map<String, Object>> contents = courseMapper.courseList(paramMap);
+        int count = courseMapper.countCourses();
+        return new PageImpl<>(contents,page,count);
+    }
+
+    @Override
+    public CourseDTO courseDetail(String courseId) {
+
+        return courseMapper
+                .courseDetail(courseId)
+                .orElseThrow(() -> new NoSuchElementException("해당 코스가 존재하지 않습니다."));
+    }
 }
