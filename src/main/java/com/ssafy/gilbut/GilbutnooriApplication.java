e924d697ac1d4b6db34dcdfee2742e23333b1b75
fly-like-you <qkrwnsgh71w@gmail.com>
2024-11-03 15:43:24 +0900

    init. init

---
+++ src/main/java/com/ssafy/gilbut/GilbutnooriApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gilbut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbutnooriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilbutnooriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/gilbut/GilbutnooriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gilbut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbutnooriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
