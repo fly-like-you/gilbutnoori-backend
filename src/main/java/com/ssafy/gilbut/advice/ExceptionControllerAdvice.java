6810c1429106d5e02a6202c75979c51c8db88353
parkjunho <qkrwnsgh71w@gmail.com>
2024-11-20 09:44:18 +0900

    feat. 핸들러 확장성 추가

--- src/main/java/com/ssafy/gilbut/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/ssafy/gilbut/advice/ExceptionControllerAdvice.java
@@ -1,10 +1,8 @@
 package com.ssafy.gilbut.advice;
 
-import com.ssafy.gilbut.advice.code.BaseErrorCode;
-import com.ssafy.gilbut.advice.status.ErrorStatus;
+import com.ssafy.gilbut.advice.code.ErrorReasonDTO;
 import com.ssafy.gilbut.exception.handler.TempHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,36 +14,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(TempHandler.class)
-	public ResponseEntity<?> handleTempHandlerException(TempHandler t, WebRequest request) {
-//		log.info(t.getMessage());
-		return handleExceptionInternalConstraint(t, t.getCode(), HttpHeaders.EMPTY, request);
-	}
-//	@ExceptionHandler(Exception.class)
-//	public ResponseEntity<?> handleAllException(Exception e, WebRequest request) {
-//		log.error("", e);
-//
-//		return handleExceptionInternalConstraint(e, ErrorStatus._INTERNAL_SERVER_ERROR, HttpHeaders.EMPTY, request);
-//	}
+	public ResponseEntity<?> handleTempHandlerException(TempHandler t) {
+		ErrorReasonDTO reason = t.getCode().getReason();
+		log.error(reason.toString());
 
-	private ResponseEntity<Object> handleExceptionInternalConstraint(Exception e, ErrorStatus errorCommonStatus,
-																	 HttpHeaders headers, WebRequest request) {
-		ApiResponse<Object> body = ApiResponse.onFailure(errorCommonStatus.getCode(), errorCommonStatus.getMessage(), null);
-		return super.handleExceptionInternal(
-				e,
-				body,
-				headers,
-				errorCommonStatus.getHttpStatus(),
-				request
-		);
+		return ResponseEntity.status(t.getCode().getReasonHttpStatus().getHttpStatus())
+				.body(ApiResponse.onFailure(reason.getCode(), reason.getMessage(), null));
+
 	}
-	private ResponseEntity<Object> handleExceptionInternalConstraint(Exception e, BaseErrorCode baseErrorCode,
-																	 HttpHeaders headers, WebRequest request) {
-		return super.handleExceptionInternal(
-				e,
-				baseErrorCode.getReason(),
-				headers,
-				baseErrorCode.getReason().getHttpStatus(),
-				request
-		);
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleAllException(Exception e, WebRequest request) {
+		log.error("", e);
+
+//		return handleExceptionInternalConstraint(e, ErrorStatus._INTERNAL_SERVER_ERROR, HttpHeaders.EMPTY, request);
+		return null;
 	}
+
 }
